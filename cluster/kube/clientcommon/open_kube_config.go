6d4a5bb3de47921d691f3b35861fea586a5639cd
Artur Troian <troian.ap@gmail.com>
2022-07-13 10:17:18 -0400

    refactor: set default value for kubeconfig flag
    expand env if any for kubeconfig path
    ref ovrclk/engineering#360
    
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- cluster/kube/clientcommon/open_kube_config.go
+++ cluster/kube/clientcommon/open_kube_config.go
@@ -2,32 +2,32 @@ package clientcommon
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/tendermint/tendermint/libs/log"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/flowcontrol"
-	"k8s.io/client-go/util/homedir"
-	"os"
-	"path"
 )
 
 func OpenKubeConfig(cfgPath string, log log.Logger) (*rest.Config, error) {
-	// If no value is specified, use a default
-	if len(cfgPath) == 0 {
-		cfgPath = path.Join(homedir.HomeDir(), ".kube", "config")
-	}
-
 	// Always bypass the default rate limiting
 	rateLimiter := flowcontrol.NewFakeAlwaysRateLimiter()
 
-	if _, err := os.Stat(cfgPath); err == nil {
-		log.Info("using kube config file", "path", cfgPath)
-		cfg, err := clientcmd.BuildConfigFromFlags("", cfgPath)
-		if err != nil {
-			return cfg, fmt.Errorf("%w: error building kubernetes config", err)
+	// if cfgPath contains value it is either set to default value $HOME/.kube/config
+	// or explicitly by env/flag AP_KUBECONFIG/--kubeconfig
+	if cfgPath != "" {
+		cfgPath = os.ExpandEnv(cfgPath)
+
+		if _, err := os.Stat(cfgPath); err == nil {
+			log.Info("using kube config file", "path", cfgPath)
+			cfg, err := clientcmd.BuildConfigFromFlags("", cfgPath)
+			if err != nil {
+				return cfg, fmt.Errorf("%w: error building kubernetes config", err)
+			}
+			cfg.RateLimiter = rateLimiter
+			return cfg, err
 		}
-		cfg.RateLimiter = rateLimiter
-		return cfg, err
 	}
 
 	log.Info("using in cluster kube config")
