ebe8bb6928178e53fe6c9c05c65c6180441030d2
Artur Troian <troian.ap@gmail.com>
2022-11-16 15:54:42 -0500

    feat(ci): send event to ovrclk/homebrew-tap once release is out (#41)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- script/is_prerelease.sh
+++ script/is_prerelease.sh
@@ -9,4 +9,4 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
-[[ -n $("${SCRIPT_DIR}"/semver.sh get prerel "$1" >/dev/null 2>&1) ]] && echo true || echo false
+[[ $("${SCRIPT_DIR}"/semver.sh get prerel "$1" 2>/dev/null) != "" ]] && echo true || echo false
