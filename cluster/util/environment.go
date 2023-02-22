99734339d9237d99b9859b5c22eb22ac11917b8b
Joao Luna <joaopedroluna98@gmail.com>
2023-02-22 19:16:37 +0000

    refactor: improve code

---
+++ cluster/util/environment.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"os"
+	"strings"
+)
+
+func EnvironmentVariablesToMap() map[string]string {
+	m := make(map[string]string)
+	for _, e := range os.Environ() {
+		if i := strings.Index(e, "="); i >= 0 {
+			m[e[:i]] = e[i+1:]
+		}
+	}
+
+	return m
+}
