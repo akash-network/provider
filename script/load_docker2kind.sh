744d269b30387d6dfd57e5ac016db293e24a15b5
Artur Troian <troian.ap@gmail.com>
2023-03-22 17:04:29 -0400

    fix: disable shellcheck for bash array
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- script/load_docker2kind.sh
+++ script/load_docker2kind.sh
@@ -3,6 +3,7 @@
 images=("$1")
 kind_name=$2
 
+# shellcheck disable=SC2048
 for image in ${images[*]}; do
     if ! docker image inspect "$image" >/dev/null 2>&1; then
         echo "image \"$image\" is not present locally"
@@ -10,6 +11,7 @@ for image in ${images[*]}; do
     fi
 done
 
+# shellcheck disable=SC2048
 for image in ${images[*]}; do
     kind --name "${kind_name}" load docker-image "${image}"
 done
