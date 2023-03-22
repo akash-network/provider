f6a6d9aa832098b59db78f98c07ffe5e0d0041f2
Artur Troian <troian.ap@gmail.com>
2023-03-22 17:06:39 -0400

    fix: disable shellcheck for bash array (#89)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- script/load_docker2ctr.sh
+++ script/load_docker2ctr.sh
@@ -9,6 +9,7 @@ if ! ssh "$remote" "which nerdctl" >/dev/null 2>&1; then
     exit 1
 fi
 
+# shellcheck disable=SC2048
 for image in ${images[*]}; do
     if ! docker image inspect "$image" >/dev/null 2>&1; then
         echo "image \"$image\" is not present locally"
@@ -17,6 +18,8 @@ for image in ${images[*]}; do
 done
 
 # need tqdm installed (https://github.com/tqdm/tqdm) for this to work.
+
+# shellcheck disable=SC2048
 for image in ${images[*]}; do
-    docker save "$image" | tqdm --bytes --total "$(docker image inspect $image --format='{{.Size}}')" | ssh "$remote" "nerdctl image load"
+    docker save "$image" | tqdm --bytes --total "$(docker image inspect "$image" --format='{{.Size}}')" | ssh "$remote" "nerdctl image load"
 done
