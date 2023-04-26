8128cc3a587d08c5a634e6a3a429337c43843baf
Artur Troian <troian.ap@gmail.com>
2023-04-26 09:37:21 -0400

    fix(_run): add install of helm charts to the cluster-setup (#114)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- _run/common-helm.mk
+++ _run/common-helm.mk
@@ -4,13 +4,13 @@ LOKI_VERSION     ?= 2.9.1
 PROMTAIL_VERSION ?= 3.11.0
 GRAFANA_VERSION  ?= 6.21.2
 
-.PHONY: kind-install-helm-charts
-kind-install-helm-charts: $(patsubst %, kind-install-helm-chart-%,$(HELM_CHARTS))
+.PHONY: kube-install-helm-charts
+kube-install-helm-charts: $(patsubst %, kube-install-helm-chart-%,$(HELM_CHARTS))
 
 # Create a kubernetes cluster with multi-tenant loki, promtail and grafana integrated for logging.
 # See: https://www.scaleway.com/en/docs/tutorials/manage-k8s-logging-loki/ for more info.
-.PHONY: kind-install-helm-chart-loki
-kind-install-helm-chart-loki:
+.PHONY: kube-install-helm-chart-loki
+kube-install-helm-chart-loki:
 	helm repo add grafana https://grafana.github.io/helm-charts
 	helm repo update
 	helm upgrade --install loki grafana/loki \
