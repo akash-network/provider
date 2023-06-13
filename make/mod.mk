282123bb30d0c57fd4c04b479cc92ea156a1d759
Artur Troian <troian.ap@gmail.com>
2023-06-13 11:29:08 -0400

    fix: build node docker image if tag is not present in registry
    helpful when node release is on the way and provider needs
    to run tests on the tag
    
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/mod.mk
+++ make/mod.mk
@@ -13,5 +13,9 @@ modsensure: deps-tidy deps-vendor
 
 .PHONY: modvendor
 modvendor: $(MODVENDOR) modsensure
+ifeq ($(GO_MOD), vendor)
+modvendor:
 	@echo "vendoring non-go files..."
 	chmod +x $(K8S_GENERATE_GROUPS)
+	$(MODVENDOR) -copy="**/Makefile **/*.sh **/*.mk" -include=github.com/akash-network/node
+endif
