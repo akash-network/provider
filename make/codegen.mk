c6c518fab92251496e4c9ce46b945c154c75b4b8
Artur Troian <troian.ap@gmail.com>
2023-06-15 12:45:37 -0400

    build: remove need for vendor
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- make/codegen.mk
+++ make/codegen.mk
@@ -1,11 +1,11 @@
 .PHONY: generate
-generate: modvendor $(MOCKERY)
+generate: $(MOCKERY)
 	$(GO) generate ./...
 
 .PHONY: kubetypes
-kubetypes: $(K8S_GENERATE_GROUPS)
+kubetypes: $(K8S_KUBE_CODEGEN)
 	rm -rf pkg/client/*
-	GOBIN=$(AP_DEVCACHE_BIN) $(K8S_GENERATE_GROUPS) all \
+	GOBIN=$(AP_DEVCACHE_BIN) $(K8S_KUBE_CODEGEN) all \
 	github.com/akash-network/provider/pkg/client github.com/akash-network/provider/pkg/apis \
 	"akash.network:v2beta1,v2beta2" \
 	--go-header-file "pkg/apis/boilerplate.go.txt"
--- make/setup-cache.mk
+++ make/setup-cache.mk
@@ -53,15 +53,6 @@ $(STATIK_VERSION_FILE): $(AP_DEVCACHE)
 	touch $@
 $(STATIK): $(STATIK_VERSION_FILE)
 
-$(MODVENDOR_VERSION_FILE): $(AP_DEVCACHE)
-	@echo "installing modvendor $(MODVENDOR_VERSION) ..."
-	rm -f $(MODVENDOR)
-	GOBIN=$(AP_DEVCACHE_BIN) $(GO) install github.com/goware/modvendor@$(MODVENDOR_VERSION)
-	rm -rf "$(dir $@)"
-	mkdir -p "$(dir $@)"
-	touch $@
-$(MODVENDOR): $(MODVENDOR_VERSION_FILE)
-
 $(GIT_CHGLOG_VERSION_FILE): $(AP_DEVCACHE)
 	@echo "installing git-chglog $(GIT_CHGLOG_VERSION) ..."
 	rm -f $(GIT_CHGLOG)
@@ -89,16 +80,17 @@ $(GOLANGCI_LINT_VERSION_FILE): $(AP_DEVCACHE)
 	touch $@
 $(GOLANGCI_LINT): $(GOLANGCI_LINT_VERSION_FILE)
 
-$(K8S_CODE_GEN_VERSION_FILE): $(AP_DEVCACHE) modvendor
+$(K8S_CODE_GEN_VERSION_FILE): $(AP_DEVCACHE)
 	@echo "installing k8s code-generator $(K8S_CODE_GEN_VERSION) ..."
 	rm -f $(K8S_GO_TO_PROTOBUF)
-	GOBIN=$(AP_DEVCACHE_BIN) go install $(ROOT_DIR)/vendor/k8s.io/code-generator/...
+	GOBIN=$(AP_DEVCACHE_BIN) go install k8s.io/code-generator/...
+	wget -q https://raw.githubusercontent.com/kubernetes/code-generator/$(K8S_CODE_GEN_VERSION)/$(K8S_KUBE_CODEGEN_FILE) -O $(K8S_KUBE_CODEGEN)
 	rm -rf "$(dir $@)"
 	mkdir -p "$(dir $@)"
 	touch $@
-	chmod +x $(K8S_GENERATE_GROUPS)
+	chmod +x $(K8S_KUBE_CODEGEN)
 $(K8S_GO_TO_PROTOBUF): $(K8S_CODE_GEN_VERSION_FILE)
-#$(K8S_GENERATE_GROUPS): $(K8S_CODE_GEN_VERSION_FILE)
+$(K8S_KUBE_CODEGEN): $(K8S_CODE_GEN_VERSION_FILE)
 
 ifeq (false, $(_SYSTEM_KIND))
 $(KIND_VERSION_FILE): $(AP_DEVCACHE)
@@ -110,20 +102,6 @@ $(KIND_VERSION_FILE): $(AP_DEVCACHE)
 $(KIND): $(KIND_VERSION_FILE)
 else
 	@echo "using alread installed kind $(KIND_VERSION) $(KIND)"
-endif
-
-$(NPM):
-ifeq (, $(shell which $(NPM) 2>/dev/null))
-	$(error "npm installation required")
-endif
-
-$(SWAGGER_COMBINE): $(AP_DEVCACHE) $(NPM)
-ifeq (, $(shell which swagger-combine 2>/dev/null))
-	@echo "Installing swagger-combine..."
-	npm install swagger-combine --prefix $(AP_DEVCACHE_NODE_MODULES)
-	chmod +x $(SWAGGER_COMBINE)
-else
-	@echo "swagger-combine already installed; skipping..."
 endif
 
 cache-clean:
