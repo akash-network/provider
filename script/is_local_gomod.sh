db7db8c7836c9d0a45338fba590c70e4bbadca3d
Artur Troian <troian.ap@gmail.com>
2023-08-30 19:03:25 -0400

    build: check node module location to determine need to build or download binary
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- script/is_local_gomod.sh
+++ script/is_local_gomod.sh
@@ -2,4 +2,4 @@
 
 MOD_PATH=$(go list -mod=readonly -m -f '{{ .Replace }}' "$1" 2>/dev/null | grep -v -x -F "<nil>")
 
-[[ "${MOD_PATH}" = /* ]] && echo -n true || echo -n false
+[[ "${MOD_PATH}" =~ ^(\/|\.\/|\.\.\/).*$ ]] && echo -n true || echo -n false
