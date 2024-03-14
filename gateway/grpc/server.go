4b08c9dcf0885fe27abae6a81fd03be817bd7f91
Andrew Hare <andrew.hare@akash.network>
2024-03-14 15:16:32 -0600

    feat: SendManifest

--- gateway/grpc/server.go
+++ gateway/grpc/server.go
@@ -1,74 +1,35 @@
 package grpc
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net"
 	"time"
 
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/peer"
-	"google.golang.org/protobuf/types/known/emptypb"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/akash-network/akash-api/go/grpc/gogoreflection"
 	ctypes "github.com/akash-network/akash-api/go/node/cert/v1beta3"
+	leasev1 "github.com/akash-network/akash-api/go/provider/lease/v1"
 	providerv1 "github.com/akash-network/akash-api/go/provider/v1"
 
 	"github.com/akash-network/provider"
 	"github.com/akash-network/provider/gateway/utils"
 	"github.com/akash-network/provider/tools/fromctx"
-	ptypes "github.com/akash-network/provider/types"
 )
 
-type ContextKey string
-
-const (
-	ContextKeyQueryClient = ContextKey("query-client")
-	ContextKeyOwner       = ContextKey("owner")
-)
-
-type grpcProviderV1 struct {
-	ctx    context.Context
-	client provider.StatusClient
-}
-
-var _ providerv1.ProviderRPCServer = (*grpcProviderV1)(nil)
-
-func QueryClientFromCtx(ctx context.Context) ctypes.QueryClient {
-	val := ctx.Value(ContextKeyQueryClient)
-	if val == nil {
-		panic("context does not have pubsub set")
-	}
-
-	return val.(ctypes.QueryClient)
-}
-
-func ContextWithOwner(ctx context.Context, address sdk.Address) context.Context {
-	return context.WithValue(ctx, ContextKeyOwner, address)
-}
-
-func OwnerFromCtx(ctx context.Context) sdk.Address {
-	val := ctx.Value(ContextKeyOwner)
-	if val == nil {
-		return sdk.AccAddress{}
-	}
-
-	return val.(sdk.Address)
-}
-
-func Serve(ctx context.Context, endpoint string, certs []tls.Certificate, client provider.StatusClient, cquery ctypes.QueryClient) error {
+func Serve(ctx context.Context, endpoint string, certs []tls.Certificate, c provider.Client) error {
 	group, err := fromctx.ErrGroupFromCtx(ctx)
 	if err != nil {
 		return err
 	}
 
 	var (
-		grpcSrv = newServer(ctx, certs, client, cquery)
+		grpcSrv = newServer(ctx, certs, c)
 		log     = fromctx.LogcFromCtx(ctx)
 	)
 
@@ -94,7 +55,17 @@ func Serve(ctx context.Context, endpoint string, certs []tls.Certificate, client
 	return nil
 }
 
-func newServer(ctx context.Context, certs []tls.Certificate, client provider.StatusClient, cquery ctypes.QueryClient) *grpc.Server {
+var (
+	_ providerv1.ProviderRPCServer = (*server)(nil)
+	_ leasev1.LeaseRPCServer       = (*server)(nil)
+)
+
+type server struct {
+	*providerV1
+	*leaseV1
+}
+
+func newServer(ctx context.Context, certs []tls.Certificate, c provider.Client) *grpc.Server {
 	// InsecureSkipVerify is set to true due to inability to use normal TLS verification
 	// certificate validation and authentication performed later in mtlsHandler
 	tlsConfig := &tls.Config{
@@ -104,27 +75,42 @@ func newServer(ctx context.Context, certs []tls.Certificate, client provider.Sta
 		MinVersion:         tls.VersionTLS13,
 	}
 
-	grpcSrv := grpc.NewServer(grpc.Creds(
-		credentials.NewTLS(tlsConfig)),
+	cquery := MustQueryClientFromCtx(ctx)
+
+	g := grpc.NewServer(
+		grpc.Creds(
+			credentials.NewTLS(tlsConfig),
+		),
 		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
 			MinTime:             30 * time.Second,
 			PermitWithoutStream: false,
 		}),
-		grpc.ChainUnaryInterceptor(mtlsInterceptor(cquery)))
+		grpc.ChainUnaryInterceptor(
+			mtlsInterceptor(cquery),
+			errorLogInterceptor(),
+		),
+	)
 
-	pRPC := &grpcProviderV1{
-		ctx:    ctx,
-		client: client,
+	s := &server{
+		providerV1: &providerV1{
+			ctx: ctx,
+			c:   c,
+		},
+		leaseV1: &leaseV1{
+			ctx: ctx,
+			c:   c,
+		},
 	}
 
-	providerv1.RegisterProviderRPCServer(grpcSrv, pRPC)
-	gogoreflection.Register(grpcSrv)
+	providerv1.RegisterProviderRPCServer(g, s)
+	leasev1.RegisterLeaseRPCServer(g, s)
+	gogoreflection.Register(g)
 
-	return grpcSrv
+	return g
 }
 
 func mtlsInterceptor(cquery ctypes.QueryClient) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, h grpc.UnaryHandler) (any, error) {
 		if p, ok := peer.FromContext(ctx); ok {
 			if mtls, ok := p.AuthInfo.(credentials.TLSInfo); ok {
 				owner, err := utils.VerifyCertChain(ctx, mtls.State.PeerCertificates, cquery)
@@ -138,33 +124,20 @@ func mtlsInterceptor(cquery ctypes.QueryClient) grpc.UnaryServerInterceptor {
 			}
 		}
 
-		return handler(ctx, req)
+		return h(ctx, req)
 	}
 }
 
-func (gm *grpcProviderV1) GetStatus(ctx context.Context, _ *emptypb.Empty) (*providerv1.Status, error) {
-	return gm.client.StatusV1(ctx)
-}
-
-func (gm *grpcProviderV1) StreamStatus(_ *emptypb.Empty, stream providerv1.ProviderRPC_StreamStatusServer) error {
-	bus, err := fromctx.PubSubFromCtx(gm.ctx)
-	if err != nil {
-		return err
-	}
-
-	events := bus.Sub(ptypes.PubSubTopicProviderStatus)
-
-	for {
-		select {
-		case <-gm.ctx.Done():
-			return gm.ctx.Err()
-		case <-stream.Context().Done():
-			return stream.Context().Err()
-		case evt := <-events:
-			val := evt.(providerv1.Status)
-			if err := stream.Send(&val); err != nil {
-				return err
-			}
+// TODO(andrewhare): Possibly replace this with
+// https://github.com/grpc-ecosystem/go-grpc-middleware/tree/main/interceptors/logging
+// to get full request/response logging?
+func errorLogInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, i *grpc.UnaryServerInfo, h grpc.UnaryHandler) (any, error) {
+		resp, err := h(ctx, req)
+		if err != nil {
+			fromctx.LogcFromCtx(ctx).Error(i.FullMethod, "err", err)
 		}
+
+		return resp, err
 	}
 }
--- gateway/grpc/server_test.go
+++ gateway/grpc/server_test.go
@@ -3,25 +3,44 @@ package grpc
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"net"
 	"testing"
 
-	qmock "github.com/akash-network/akash-api/go/node/client/v1beta2/mocks"
-	providerv1 "github.com/akash-network/akash-api/go/provider/v1"
-	"github.com/akash-network/node/testutil"
-	"github.com/akash-network/provider/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	manifestValidation "github.com/akash-network/akash-api/go/manifest/v2beta2"
+	types "github.com/akash-network/akash-api/go/node/cert/v1beta3"
+	qmock "github.com/akash-network/akash-api/go/node/client/v1beta2/mocks"
+	leasev1 "github.com/akash-network/akash-api/go/provider/lease/v1"
+	providerv1 "github.com/akash-network/akash-api/go/provider/v1"
+	"github.com/akash-network/node/testutil"
+
+	pmanifest "github.com/akash-network/provider/manifest"
+	mmocks "github.com/akash-network/provider/manifest/mocks"
+	"github.com/akash-network/provider/mocks"
 )
+
+type asserter interface {
+	AssertExpectations(mock.TestingT) bool
+}
+
+type client struct {
+	p providerv1.ProviderRPCClient
+	l leasev1.LeaseRPCClient
+}
 
 func TestRPCs(t *testing.T) {
 	var (
-		qclient qmock.QueryClient
-		com     = testutil.CertificateOptionMocks(&qclient)
+		qclient = &qmock.QueryClient{}
+		com     = testutil.CertificateOptionMocks(qclient)
 		cod     = testutil.CertificateOptionDomains([]string{"localhost", "127.0.0.1"})
 	)
 
@@ -30,21 +49,118 @@ func TestRPCs(t *testing.T) {
 		crt2 = testutil.Certificate(t, testutil.AccAddress(t), com, cod)
 	)
 
+	qclient.EXPECT().Certificates(mock.Anything, mock.Anything).Return(&types.QueryCertificatesResponse{
+		Certificates: types.CertificatesResponse{
+			types.CertificateResponse{
+				Certificate: types.Certificate{
+					State:  types.CertificateValid,
+					Cert:   crt2.PEM.Cert,
+					Pubkey: crt2.PEM.Pub,
+				},
+				Serial: crt2.Serial.String(),
+			},
+		},
+	}, nil)
+
 	cases := []struct {
-		desc         string
-		statusClient func() *mocks.StatusClient
-		run          func(context.Context, *testing.T, providerv1.ProviderRPCClient)
+		desc  string
+		mocks func() (*mocks.Client, []asserter)
+		run   func(context.Context, *testing.T, client)
 	}{
 		{
 			desc: "GetStatus",
-			statusClient: func() *mocks.StatusClient {
-				var m mocks.StatusClient
-				m.EXPECT().StatusV1(mock.AnythingOfType("context.Context")).Return(nil, nil)
-				return &m
+			mocks: func() (*mocks.Client, []asserter) {
+				var c mocks.Client
+				c.EXPECT().StatusV1(mock.Anything).Return(&providerv1.Status{}, nil)
+				return &c, nil
 			},
-			run: func(ctx context.Context, t *testing.T, c providerv1.ProviderRPCClient) {
-				_, err := c.GetStatus(ctx, &emptypb.Empty{})
+			run: func(ctx context.Context, t *testing.T, c client) {
+				_, err := c.p.GetStatus(ctx, &emptypb.Empty{})
 				assert.NoError(t, err)
+			},
+		},
+		{
+			desc: "SendManifest",
+			mocks: func() (*mocks.Client, []asserter) {
+				var (
+					c  mocks.Client
+					mc mmocks.Client
+				)
+
+				mc.EXPECT().Submit(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+				c.EXPECT().Manifest().Return(&mc)
+
+				return &c, []asserter{&mc}
+			},
+			run: func(ctx context.Context, t *testing.T, c client) {
+				_, err := c.l.SendManifest(ctx, &leasev1.SendManifestRequest{})
+				assert.NoError(t, err)
+			},
+		},
+		{
+			desc: "SendManifest invalid",
+			mocks: func() (*mocks.Client, []asserter) {
+				var (
+					c  mocks.Client
+					mc mmocks.Client
+				)
+
+				mc.EXPECT().Submit(mock.Anything, mock.Anything, mock.Anything).Return(manifestValidation.ErrInvalidManifest)
+				c.EXPECT().Manifest().Return(&mc)
+
+				return &c, []asserter{&mc}
+			},
+			run: func(ctx context.Context, t *testing.T, c client) {
+				_, err := c.l.SendManifest(ctx, &leasev1.SendManifestRequest{})
+				assert.ErrorContains(t, err, "invalid manifest")
+
+				s, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, codes.InvalidArgument, s.Code())
+			},
+		},
+		{
+			desc: "SendManifest no lease",
+			mocks: func() (*mocks.Client, []asserter) {
+				var (
+					c  mocks.Client
+					mc mmocks.Client
+				)
+
+				mc.EXPECT().Submit(mock.Anything, mock.Anything, mock.Anything).Return(pmanifest.ErrNoLeaseForDeployment)
+				c.EXPECT().Manifest().Return(&mc)
+
+				return &c, []asserter{&mc}
+			},
+			run: func(ctx context.Context, t *testing.T, c client) {
+				_, err := c.l.SendManifest(ctx, &leasev1.SendManifestRequest{})
+				assert.ErrorContains(t, err, "no lease")
+
+				s, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, codes.NotFound, s.Code())
+			},
+		},
+		{
+			desc: "SendManifest internal",
+			mocks: func() (*mocks.Client, []asserter) {
+				var (
+					c  mocks.Client
+					mc mmocks.Client
+				)
+
+				mc.EXPECT().Submit(mock.Anything, mock.Anything, mock.Anything).Return(errors.New("boom"))
+				c.EXPECT().Manifest().Return(&mc)
+
+				return &c, []asserter{&mc}
+			},
+			run: func(ctx context.Context, t *testing.T, c client) {
+				_, err := c.l.SendManifest(ctx, &leasev1.SendManifestRequest{})
+				assert.ErrorContains(t, err, "boom")
+
+				s, ok := status.FromError(err)
+				assert.True(t, ok)
+				assert.Equal(t, codes.Internal, s.Code())
 			},
 		},
 	}
@@ -56,12 +172,16 @@ func TestRPCs(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
-			ctx = context.WithValue(ctx, ContextKeyQueryClient, &qclient)
+			ctx = ContextWithQueryClient(ctx, qclient)
 
-			statusClient := c.statusClient()
-			statusClient.AssertExpectations(t)
+			mc, as := c.mocks()
+			defer mc.AssertExpectations(t)
 
-			s := newServer(ctx, crt1.Cert, statusClient, &qclient)
+			for _, a := range as {
+				defer a.AssertExpectations(t)
+			}
+
+			s := newServer(ctx, crt1.Cert, mc)
 			defer s.Stop()
 
 			l, err := net.Listen("tcp", ":0")
@@ -82,7 +202,10 @@ func TestRPCs(t *testing.T) {
 
 			defer conn.Close()
 
-			c.run(ctx, t, providerv1.NewProviderRPCClient(conn))
+			c.run(ctx, t, client{
+				p: providerv1.NewProviderRPCClient(conn),
+				l: leasev1.NewLeaseRPCClient(conn),
+			})
 		})
 	}
 }
