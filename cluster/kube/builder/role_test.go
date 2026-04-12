fe568f9b283a50ddd1e7b7dc7a36137d157807c8
Joao Luna <7607329+cloud-j-luna@users.noreply.github.com>
2026-04-12 14:24:31 +0100

    test: execute skipped tests (#390)

--- cluster/kube/builder/role_test.go
+++ cluster/kube/builder/role_test.go
@@ -12,8 +12,6 @@ import (
 )
 
 func TestRolePolicyRules(t *testing.T) {
-	t.Skip("skipping test. flapping on GH actions")
-
 	log := testutil.Logger(t)
 	const fakeHostname = "ahostname.dev"
 	settings := Settings{
